b26bc3793e94842f766da35f6826cec77a6126c7
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-04-19 21:50:39 +0300

    edited vercel.json

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,22 +1,47 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from '../../redux/auth/selectors';
-import { logOut } from '../../redux/auth/operations';
-import css from './UserMenu.module.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectUser } from '../../redux/auth/selectors';
+// import { logOut } from '../../redux/auth/operations';
+// import css from './UserMenu.module.css';
 
-export default function UserMenu() {
+// export default function UserMenu() {
+//   const dispatch = useDispatch();
+//   const user = useSelector(selectUser);
+
+//   const handleLogout = () => {
+//     dispatch(logOut());
+//   };
+
+//   return (
+//     <div className={css.wrapper}>
+//       <p className={css.username}>Welcome, {user.name}</p>
+//       <button type="button" onClick={handleLogout}>
+//         Logout
+//       </button>
+//     </div>
+//   );
+// }
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../../redux/auth/selectors";
+import { logOut } from "../../redux/auth/operations";
+import css from "./UserMenu.module.css";
+
+export const UserMenu = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
 
-  const handleLogout = () => {
+  const handlLogOut = () => {
     dispatch(logOut());
   };
 
+  // Якщо user ще не завантажився — нічого не показуємо або спінер
+  if (!user) return null;
+
   return (
-    <div className={css.wrapper}>
-      <p className={css.username}>Welcome, {user.name}</p>
-      <button type="button" onClick={handleLogout}>
+    <div className={css.el}>
+      <p className={css.name}>Welcome, {user.name}</p>
+      <button className={css.btn} type="button" onClick={handlLogOut}>
         Logout
       </button>
     </div>
   );
-}
+};
